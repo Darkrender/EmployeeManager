4bf5e28c6d041a6452e1e0709d7f35dc78b61246
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 00:16:13 -0600

    Create Employee model

---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Wage { get; set; }
+
+        public bool IsSalaried { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public int PayFrequency { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
