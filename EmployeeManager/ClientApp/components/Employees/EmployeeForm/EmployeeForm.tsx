c0ed22893a50db330370b0acf9366dcc10eb5eee
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 13:11:31 -0600

    Create EmployeeForm component

---
+++ EmployeeManager/ClientApp/components/Employees/EmployeeForm/EmployeeForm.tsx
@@ -0,0 +1,34 @@
+﻿import * as React from 'react';
+import { connect, Dispatch, Store } from 'react-redux';
+import { ApplicationState } from 'ClientApp/store';
+import * as EmployeeState from '../../../store/Employees';
+import { Employee } from 'ClientApp/shared/interfaces/Employee';
+
+type EmployeeListProps = {
+    selectedEmployee: Employee
+};
+
+class EmployeeList extends React.Component<EmployeeListProps, {}> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        const { selectedEmployee } = this.props;
+
+        return (
+            <div>
+                Employee Form
+
+                <div>{selectedEmployee ? selectedEmployee : "No one selected"}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: ApplicationState) => {
+    const { selectedEmployee } = state.employees
+    return { selectedEmployee };
+}
+
+export default connect(mapStateToProps, EmployeeState.actionCreators)(EmployeeList);
