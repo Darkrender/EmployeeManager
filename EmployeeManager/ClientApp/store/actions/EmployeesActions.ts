a576d854929b83d2d52e32e5765c67400deeb23c
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 11:00:12 -0600

    Create EmployeeActions

---
+++ EmployeeManager/ClientApp/store/actions/EmployeesActions.ts
@@ -0,0 +1,24 @@
+﻿import { ThunkAction } from "redux-thunk";
+import { Dispatch } from "react-redux";
+import { ApplicationState, AppThunkAction } from "ClientApp/store";
+import { Employee } from "ClientApp/shared/interfaces/Employee";
+import axios, { AxiosPromise } from 'axios';
+
+export const startFetchingEmployees = (dispatch: Dispatch<ApplicationState>) => {
+    dispatch({ type: 'FETCHING_EMPLOYEES' });
+
+    return getAllEmployees().then(
+        result => {
+            dispatch(getReceivedEmployeesSuccess(result.data))
+        },
+        error => console.log(error)
+    );
+}
+
+export const getReceivedEmployeesSuccess = (payload: Employee[]) => {
+    return { type: 'GET_REVEIVED_EMPLOYEES_SUCCESS', payload };
+}
+
+export const getAllEmployees = () => {
+    return axios.get('/api/employee');
+}
