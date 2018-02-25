b3ea3fd179ae9a7544de67b0be3d5b544cc80065
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 11:46:23 -0600

    Add employees state to application state

--- EmployeeManager/ClientApp/store/index.ts
+++ EmployeeManager/ClientApp/store/index.ts
@@ -1,12 +1,15 @@
+import * as Employees from './Employees';
 
 // The top-level state object
 export interface ApplicationState {
+    employees: Employees.EmployeesState;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
+    employees: Employees.reducer
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
