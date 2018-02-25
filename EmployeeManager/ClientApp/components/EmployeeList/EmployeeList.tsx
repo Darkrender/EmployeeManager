e6f519a9b7877aad59651dd7a4cfc399862b92b4
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 00:30:12 -0600

    Create EmployeeList component

---
+++ EmployeeManager/ClientApp/components/EmployeeList/EmployeeList.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from 'react';
+import { connect, Dispatch, Store } from 'react-redux';
+import { ApplicationState } from 'ClientApp/store';
+
+class EmployeeList extends React.Component {
+    render() {
+        return (
+            <div>Employee List</div>
+        );
+    }
+}
