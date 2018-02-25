5ecc2382ca335a30076193a4ad844adaabbddd59
Darkrender <jimmiegisclair@gmail.com>
2018-02-25 11:43:46 -0600

    Set up initial db migration

---
+++ EmployeeManager/Migrations/20180225173853_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeeManager.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    IsSalaried = table.Column<bool>(nullable: false),
+                    JobTitle = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    PayFrequency = table.Column<int>(nullable: false),
+                    Wage = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
