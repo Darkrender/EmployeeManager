ddf6eb7ebafe22f95369da06e61641b7c53bae6d
Jimmie Gisclair <jimmiegisclair@gmail.com>
2018-02-26 17:35:29 -0600

    Update employee form component to bind it to the new employee form state

--- EmployeeManager/ClientApp/components/Employees/EmployeeForm/EmployeeForm.jsx
+++ EmployeeManager/ClientApp/components/Employees/EmployeeForm/EmployeeForm.jsx
@@ -1,30 +1,63 @@
 ﻿import React, { Component } from 'react';
 import { connect, Dispatch, Store } from 'react-redux';
-import * as EmployeeState from '../../../store/reducers/Employees';
+import { actionCreators } from '../../../store/reducers/EmployeeForm';
 
 class EmployeeForm extends Component {
     constructor(props) {
         super(props);
+
+        this._handleChange = this._handleChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
+    }
+
+    _handleChange(event) {
+        this.props.beginUpdateForm({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+
+        this.props.selectedEmployee ? this.props.updateSelectedEmployee() : this.props.createEmployee();
+    }
+
+    _displaySubmitBtn() {
+        console.log('Selected Employee: ' + this.props.selectedEmployee);
+        if (this.props.selectedEmployee) {
+            return (
+                <a href="#" className="update-employee-btn">
+                    <i className="fa fa-plus"></i> <span> Update Employee</span>
+                </a>
+            );
+        }
+
+        return (
+            <a href="#" className="create-employee-btn">
+                <i className="fa fa-plus"></i> <span> Create Employee</span>
+            </a>
+        );
     }
 
     render() {
-        const { selectedEmployee } = this.props;
+        const { id, wage, payFrequency, jobTitle, hireDate, name, isSalaried } = this.props;
 
         return (
             <div className="employee-form-container card">
                 <div className="employee-form-header card-header"></div>
 
                 <div className="card-body">
-                    <form className="employee-form">
-                        <input placeholder="Enter a name..." value={selectedEmployee ? selectedEmployee.name : ''}/>
-                        <input placeholder="Enter a job title..." value={selectedEmployee ? selectedEmployee.jobTitle : ''}/>
-                        <input placeholder="Enter a wage..." value={selectedEmployee ? selectedEmployee.wage : ''}/>
-                        <input placeholder="Select a pay frequency..." value={selectedEmployee ? selectedEmployee.payFrequency : ''}/>
-                        <input placeholder="Enter a hire date..." value={selectedEmployee ? selectedEmployee.hireDate : ''}/>
+                    <form className="employee-form" onSubmit={this._handleSubmit}>
+                        <input name="name" placeholder="Enter a name..." value={name} onChange={this._handleChange} />
+                        <input name="jobTitle" placeholder="Enter a job title..." value={jobTitle} onChange={this._handleChange} />
+                        <input name="wage" placeholder="Enter a wage..." value={wage} onChange={this._handleChange} />
+                        <input name="payFrequency" placeholder="Select a pay frequency..." value={payFrequency} onChange={this._handleChange} />
+                        <input name="hireDate" placeholder="Enter a hire date..." value={hireDate} onChange={this._handleChange} />
                     </form>
                 </div>
 
                 <div className="employee-form-footer card-footer">
+                    {this._displaySubmitBtn()}
                 </div>
             </div>
         );
@@ -32,8 +65,10 @@ class EmployeeForm extends Component {
 }
 
 const mapStateToProps = (state) => {
-    const { selectedEmployee } = state.employees
-    return { selectedEmployee };
+    console.log(state);
+    const { id, wage, payFrequency, jobTitle, hireDate, name, isSalaried } = state.employeeForm;
+    const { selectedEmployee } = state.employees;
+    return { id, wage, payFrequency, jobTitle, hireDate, name, isSalaried, selectedEmployee };
 }
 
-export default connect(mapStateToProps, EmployeeState.actionCreators)(EmployeeForm);
+export default connect(mapStateToProps, actionCreators)(EmployeeForm);
