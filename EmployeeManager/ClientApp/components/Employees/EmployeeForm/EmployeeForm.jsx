05616e89e257a704f4f5c64e0d3a58f5a5d4df11
Darkrender <jimmiegisclair@gmail.com>
2018-02-26 01:12:45 -0600

    Create a form group in EmployeeForm and populate it
    If there is a selected employee in the state, the form is now populated with its information

--- EmployeeManager/ClientApp/components/Employees/EmployeeForm/EmployeeForm.jsx
+++ EmployeeManager/ClientApp/components/Employees/EmployeeForm/EmployeeForm.jsx
@@ -1,7 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { connect, Dispatch, Store } from 'react-redux';
 import * as EmployeeState from '../../../store/reducers/Employees';
-//import { Card } from 'material-ui/Card';
 
 class EmployeeForm extends Component {
     constructor(props) {
@@ -12,10 +11,21 @@ class EmployeeForm extends Component {
         const { selectedEmployee } = this.props;
 
         return (
-            <div className="employee-form-container panel">
-                <div className="employee-form-header panel-heading"></div>
+            <div className="employee-form-container card">
+                <div className="employee-form-header card-header"></div>
 
-                <div className="panel-body">{selectedEmployee ? selectedEmployee : "No one selected"}</div>
+                <div className="card-body">
+                    <form className="employee-form">
+                        <input placeholder="Enter a name..." value={selectedEmployee ? selectedEmployee.name : ''}/>
+                        <input placeholder="Enter a job title..." value={selectedEmployee ? selectedEmployee.jobTitle : ''}/>
+                        <input placeholder="Enter a wage..." value={selectedEmployee ? selectedEmployee.wage : ''}/>
+                        <input placeholder="Select a pay frequency..." value={selectedEmployee ? selectedEmployee.payFrequency : ''}/>
+                        <input placeholder="Enter a hire date..." value={selectedEmployee ? selectedEmployee.hireDate : ''}/>
+                    </form>
+                </div>
+
+                <div className="employee-form-footer card-footer">
+                </div>
             </div>
         );
     }
