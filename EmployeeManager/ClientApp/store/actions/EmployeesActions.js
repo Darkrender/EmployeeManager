2318c185301433983cc6da6479428a9ceaf3b337
Darkrender <jimmiegisclair@gmail.com>
2018-02-26 22:49:42 -0600

    Refactor beginCreateEmployee action

--- EmployeeManager/ClientApp/store/actions/EmployeesActions.js
+++ EmployeeManager/ClientApp/store/actions/EmployeesActions.js
@@ -6,19 +6,20 @@ export const startFetchingEmployees = (dispatch) => {
     return getAllEmployees().then(
         response => {
             const employees = response.data.result;
+            employees.forEach(item => item.hireDate = new Date(item.hireDate));
             dispatch(getReceivedEmployeesSuccess(employees))
         },
         error => console.log(error)
     );
 }
 
-export const beginCreateEmployee = (employee) => {
-    return (dispatch) => {
-        createEmployee(employee).then(
-            response => dispatch(addEmployeeToState(response.data.result)),
-            error => console.error(error)
-        );
-    }
+export const beginCreateEmployee = (dispatch, getState) => {
+    const newEmployee = getState().employeeForm;
+
+    return createEmployee(newEmployee).then(
+        response => dispatch(addEmployeeToState(response.data)),
+        error => console.error(error)
+    );
 }
 
 export const beginUpdateEmployee = (dispatch) => {
